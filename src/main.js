3b05bc7dde20c49dca77e7cdc119ec7964fd704a
Marwane Benharoun <71384548+MBenharoun@users.noreply.github.com>
2021-02-21 21:59:23 +0100

    JS

---
+++ src/main.js
@@ -0,0 +1,20 @@
+function onDeath(){
+    open('death.html',"_self");
+}
+function restart(){
+    open('game.html',"_self");
+}
+function startG(){
+    open("start.html","_self");
+
+}
+function startgame(){
+    setTimeout(restart,5000);
+}
+function credit(){
+    open('index.html',"_self");
+}
+function go(){
+
+    open('game.html',"_self");
+}
