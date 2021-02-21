40cb416792aa17a46485dc8a72b84fdceb473b7b
Marwane Benharoun <71384548+MBenharoun@users.noreply.github.com>
2021-02-21 16:29:31 +0100

    Redirection For HTML

---
+++ Sumoto/src/main.js
@@ -0,0 +1,16 @@
+function onDeath(){
+    open('death.html',"_self");
+}
+function restart(){
+    open('game.html',"_self");
+}
+function startG(){
+    open("start.html","_self");
+
+}
+function startgame(){
+    setTimeout(restart,5000);
+}
+function credit(){
+    open('index.html',"_self");
+}
