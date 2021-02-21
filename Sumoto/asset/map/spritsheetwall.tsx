02f524a6e0106454ec080d6342dceffbd62705d8
Marwane Benharoun <71384548+MBenharoun@users.noreply.github.com>
2021-02-21 16:04:47 +0100

    tiles set for tiled

---
+++ Sumoto/asset/map/spritsheetwall.tsx
@@ -0,0 +1,177 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="spritsheetwall" tilewidth="50" tileheight="50" tilecount="17" columns="17">
+ <grid orientation="isometric" width="10" height="10"/>
+ <image source="../mur/spritsheetwall.png" trans="ffffff" width="850" height="50"/>
+ <tile id="1">
+  <objectgroup draworder="index" id="2">
+   <object id="2" x="-45.2609" y="-35.1739" visible="0">
+    <properties>
+     <property name="collide" type="bool" value="true"/>
+    </properties>
+    <polygon points="0,0 2.26087,2.26087 52.2174,-47.8696 50.2609,-49.7391"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="2">
+  <objectgroup draworder="index" id="2">
+   <object id="3" x="53.25" y="-33.25" visible="0">
+    <properties>
+     <property name="collide" type="bool" value="true"/>
+    </properties>
+    <polygon points="0,0 -50.25,-50 -48.25,-51.75 1.625,-2.125"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="3">
+  <objectgroup draworder="index" id="2">
+   <object id="2" x="-42.625" y="-32.625" visible="0">
+    <properties>
+     <property name="collide" type="bool" value="true"/>
+    </properties>
+    <polygon points="0,0 31.75,-32.5 44.125,-44.625 47.875,-48.375 49.75,-45.75 54.75,-41.5 75.125,-20.625 82.875,-12.625 92.75,-3 95.75,0 97.875,-2.125 86.375,-13.875 79.25,-21.25 66,-33.75 49.75,-49.75 47.625,-51.875 23.875,-27.875 9.25,-13.25 0.875,-4.875 -2,-2.25"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="4">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="3.25" y="12.75" visible="0">
+    <properties>
+     <property name="collide" type="bool" value="true"/>
+    </properties>
+    <polygon points="0,0 2,2 51.5,-47.5 49.25,-50"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="5">
+  <objectgroup draworder="index" id="2">
+   <object id="2" x="2.875" y="13.125" visible="0">
+    <properties>
+     <property name="collide" type="bool" value="true"/>
+    </properties>
+    <polygon points="0,0 50.25,-50 52.25,-48 2,2.25"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="6">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="3.25" y="13" visible="0">
+    <properties>
+     <property name="collide" type="bool" value="true"/>
+    </properties>
+    <polygon points="0,0 25.75,-25.25 48.5,-48 16,-80 1.77636e-15,-96 1.875,-97.625 25.625,-74.875 51.625,-48.625 24.375,-20.375 1.875,2.125"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="7">
+  <objectgroup draworder="index" id="2">
+   <object id="2" x="-45" y="-34.75" visible="0">
+    <properties>
+     <property name="collide" type="bool" value="true"/>
+    </properties>
+    <polygon points="0,0 50,-50 99.5,-0.75 50.25,49.75 48.375,47.875 96.375,-0.125 50.375,-45.875 2,1.75"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="8">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="-45" y="-34.75" visible="0">
+    <properties>
+     <property name="collide" type="bool" value="true"/>
+    </properties>
+    <polygon points="0,0 1.75,-2.75 51.75,48 49.75,49.5"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="9">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="7" y="13" visible="0">
+    <properties>
+     <property name="collide" type="bool" value="true"/>
+    </properties>
+    <polygon points="0,0 -47.875,-47.625 0.125,-96.125 -2.75,-98 -52,-48.25 -2.125,1.625"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="10">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="-45.125" y="-34.625" visible="0">
+    <properties>
+     <property name="collide" type="bool" value="true"/>
+    </properties>
+    <polygon points="0,0 2.375,-2.375 52.25,47 49.625,49.875"/>
+   </object>
+   <object id="2" x="2.5" y="-82.25" visible="0">
+    <properties>
+     <property name="collide" type="bool" value="true"/>
+    </properties>
+    <polygon points="0,0 3.5,-3.5 52.75,46.5 50.625,49.125"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="11">
+  <properties>
+   <property name="collide" type="bool" value="false"/>
+  </properties>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="6.625" y="12.875" visible="0">
+    <properties>
+     <property name="collide" type="bool" value="true"/>
+    </properties>
+    <polygon points="0,0 -47.75,-47.75 -1.375,-93.625 46.125,-46.125 48.125,-47.625 -1.375,-97.625 -51.5,-47.75 -2,2"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="12">
+  <properties>
+   <property name="collide" type="bool" value="false"/>
+  </properties>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="-44.625" y="-34.625" visible="0">
+    <properties>
+     <property name="collide" type="bool" value="true"/>
+    </properties>
+    <polygon points="0,0 1.875,-2.125 49.625,45.875 97.5,-2 99.25,-0.25 49.625,50.125"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="13">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="7.125" y="-83.125" visible="0">
+    <properties>
+     <property name="collide" type="bool" value="true"/>
+    </properties>
+    <polygon points="0,0 -2.375,-2.125 -52,47.75 -2.375,97.875 47.875,47.875 46,46 -2.125,93.875 -48,48"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="14">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="-45.625" y="-34.875" visible="0">
+    <properties>
+     <property name="collide" type="bool" value="true"/>
+    </properties>
+    <polygon points="0,0 3.25,-1.75 50.625,45.875 96.25,-0.25 48.125,-47.875 50.625,-50.125 101,-0.25 50.875,49.875"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="15">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="-44.625" y="-34.375" visible="0">
+    <properties>
+     <property name="collide" type="bool" value="true"/>
+    </properties>
+    <polygon points="0,0 49.75,-50.25 99.75,-1 49.375,49.125"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="16">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="-44.25" y="-34.75" visible="0">
+    <properties>
+     <property name="collide" type="bool" value="true"/>
+    </properties>
+    <polygon points="0,0 49.5,-49.5 98.75,-1 49,49.75"/>
+   </object>
+  </objectgroup>
+ </tile>
+</tileset>
